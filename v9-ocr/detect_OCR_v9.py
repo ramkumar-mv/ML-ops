fc2365d172a883e11d11071bcac8a7645a064c15
ramkumar-mv <ramk78317@gmail.com>
2024-09-13 16:39:00 +0530

    changes 1

--- v9-ocr/detect_OCR_v9.py
+++ v9-ocr/detect_OCR_v9.py
@@ -400,5 +400,5 @@ if __name__ == "__main__":
         half=False
     )
     model = tf.keras.models.load_model('/Users/ramkumarmv/Desktop/iocl-ml/Fresh-Skew-CNN.h5')
-    print('OCR Model Loaded Successfully - dev branch changes before merge')
+    print('OCR Model Loaded Successfully - dev branch changes - 1')
     main()
